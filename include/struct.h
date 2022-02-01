5b8c22c65ddc642929d4d5bef4d172cb4a7ce72b
LayBraid <clement.loeuillet@epitech.eu>
2022-02-01 18:26:26 +0100

    add struct

---
+++ include/struct.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** my_sokoban
+** File description:
+** No file there , just an epitech header example
+*/
+
+#ifndef MY_SOKOBAN_STRUCT_H
+    #define MY_SOKOBAN_STRUCT_H
+
+typedef struct sokoban_struct {
+    char *buffer;
+    int length;
+    int lines;
+} sokoban_t;
+
+#endif
