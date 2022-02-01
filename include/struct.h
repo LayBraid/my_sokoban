c6482e42cb11f92110cc54d2a07167a00cdbd1e2
LayBraid <clement.loeuillet@epitech.eu>
2022-02-01 18:38:22 +0100

    add position struct

--- include/struct.h
+++ include/struct.h
@@ -8,11 +8,17 @@
 #ifndef MY_SOKOBAN_STRUCT_H
     #define MY_SOKOBAN_STRUCT_H
 
+typedef struct position_struct {
+    int x;
+    int y;
+} pos_t;
+
 typedef struct sokoban_struct {
     char *buffer;
     int length;
     int lines;
     char **map;
+    pos_t *pos;
 } sokoban_t;
 
 #endif
