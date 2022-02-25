57851ade9fe2419667ba36479e1d7dd01dcbb26c
LayBraid <clement.loeuillet@epitech.eu>
2022-02-25 20:50:58 +0100

    patch no file

--- src/check_file.c
+++ src/check_file.c
@@ -12,6 +12,8 @@ int sanity_check(char *path)
 {
     char *buffer = get_in_buffer(path);
 
+    if (my_strcmp(buffer, "error") == 0)
+        return 84;
     for (int i = 0; buffer[i] != '\0'; i++)
         if (!my_str_contains_c(buffer[i], " \n#XOP"))
             return 84;
